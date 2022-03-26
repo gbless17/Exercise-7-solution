1118a6dac3010748ebe9e7d3bad9fc552f496cfd
Gbless Ugo <gbless1720@gmail.com>
2022-03-26 16:18:04 +0100

    Ex7 solution

---
+++ Ex 7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex_7
+{
+    // Write a program, which creates an array of 20 elements of type integer and initializes
+    // each of the elements with a value equals to the index of the element multiplied by 5.
+    // Print the elements to the console.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[20];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * 5;
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+    }
+}
---
+++ Ex7_7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ex7_7
+{   // Write a program, which reads from the console two integer numbers N and K (K < N)
+    // and array of N integers. Find those K consecutive elements in the array, which have maximal sum.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            Console.Write("Enter N: ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter K (K < N): ");
+            int k = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(arr, (a, b) => b.CompareTo(a));
+
+            for (int i = 0; i < k; i++) sum += arr[i];
+
+            Console.WriteLine("\nBiggest sum is {0}", sum);
+        }
+    }
+}
